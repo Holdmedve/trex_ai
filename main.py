5a23578cb87812757fff8a17a46e7d00de4ba2e9
Holdmedve <talos.boti@gmail.com>
2022-10-03 20:59:20 +0200

    🎉 trex jumps using pynput

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from selenium import webdriver
 from time import sleep
+from pynput.keyboard import Key, Controller
 
 driver = webdriver.Firefox()
-sleep(1)
 driver.get('https://dinorunner.com')
 
 assert driver.title == "Dinosaur T-Rex Game - Chrome Dino Runner Online"
 
 print('hello trex')
 sleep(1)
+
+keyboard = Controller()
+
+# Press and release space
+keyboard.press(Key.space)
+keyboard.release(Key.space)
+sleep(1)
+# Press and release space
+keyboard.press(Key.space)
+keyboard.release(Key.space)
+sleep(1)
+# Press and release space
+keyboard.press(Key.space)
+keyboard.release(Key.space)
+
+sleep(1)
+
