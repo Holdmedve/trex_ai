9b5af203df2734d970b92aca6332b2e2393777ab
Holdmedve <talos.boti@gmail.com>
2022-10-06 12:45:22 +0200

    🧑‍💻 Add retry logic when loading page

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from time import sleep
 from pynput.keyboard import Key, Controller
 
 def load_game():
     print('opening browser...')
     driver = webdriver.Firefox()
-    sleep(3)
-    
-    print('loading page...')
-    driver.get('https://dinorunner.com')
-    print('page load finished')
+    print('browser opened')
+    driver.set_page_load_timeout(10)
+
+    retry_cnt = 0
+    while True:
+        try:
+            print('loading page...')
+            driver.get('https://dinorunner.com')
+            print('page load finished')
+            break
+        except TimeoutException as e:
+            # driver.delete_all_cookies()
+            if retry_cnt < 3:
+                print('page did not load, retrying...')
+                retry_cnt += 1
+            else:
+                print('page did not load, retries threshold exceeded')
+                raise e    
 
     assert driver.title == "Dinosaur T-Rex Game - Chrome Dino Runner Online"
 
 
 def jump(controller):
+    print('jump!')
     controller.press(Key.space)
     controller.release(Key.space)
 
