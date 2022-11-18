12f512d6b6a4c5a1a95831fe51856029ea4d6a4a
Holdmedve <talos.boti@gmail.com>
2022-11-18 20:00:21 +0100

    🎨 Use game context manager

--- scripts/main.py
+++ scripts/main.py
@@ -1,29 +1,25 @@
-
-from time import sleep, time
+from time import time
 from pynput.keyboard import Controller
 
-from game import close_game, load_game
+from game import game_context
 from random_agent import act_randomly
-from controls import Actions, crouch, jump_to_start, keep_view_on_top
+from controls import Actions, jump_to_start, keep_view_on_top
 
 
 def main():
-    webdriver = load_game()
-    sleep(1)
+    with game_context():
+        controller = Controller()
+        keep_view_on_top(controller=controller)
+        jump_to_start(controller=controller)
 
-    controller = Controller()
-    keep_view_on_top(controller=controller)
-    jump_to_start(controller=controller)    
+        prev_action, cur_action = Actions.DO_NOTHING, Actions.DO_NOTHING
+        start_time = time()
+        playtime_seconds = 7
 
-    prev_action, cur_action = Actions.DO_NOTHING, Actions.DO_NOTHING
-    start_time = time()
-    playtime_seconds = 7
+        while time() - start_time < playtime_seconds:
+            cur_action = act_randomly(prev_action=prev_action, controller=controller)
+            prev_action = cur_action
 
-    while time() - start_time < playtime_seconds:
-        cur_action = act_randomly(prev_action=prev_action, controller=controller)
-        prev_action = cur_action
 
-    close_game(webdriver=webdriver)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
